00c5358ea1e5b4abacec6fddf68d50a448d9ce50
zeljkosoko <zex.sokolovic@gmail.com>
2020-06-22 14:51:36 +0200

    indexedDB storage and readme updated

--- js/cocktailDB.js
+++ js/cocktailDB.js
@@ -1,11 +1,38 @@
-export class CoctailDB {
+var db;
+var dbRequest = indexedDB.open('FavoriteCocktails', 1);
 
+dbRequest.onsuccess = function(event) {
+    db = event.target.result;
+};
+
+dbRequest.onupgradeneeded = function(event) {
+    db = event.target.result;
+
+    const objStore = db.createObjectStore('cocktails', {keyPath: 'id'});
+
+    objStore.transaction.oncomplete = function() {
+        const favCocktails = db.transaction('cocktails','readwrite').objectStore('cocktails');
+    };
+};
+
+dbRequest.onerror = function(event){
+    console.log('ERROR');
+};
+
+export class CoctailDB {
+   
     saveToLs(cocktail) {
         const cocktails = this.getCoctailsFromLS();
         cocktails.push(cocktail);
         localStorage.setItem('cocktails', JSON.stringify(cocktails));
         sessionStorage.setItem('cocktails', JSON.stringify(cocktails));
         document.cookie = `cocktails=${JSON.stringify(cocktails)}; max-age=10`;
+
+        if(!db){
+            return;
+        }
+        const favCocktails = db.transaction('cocktails','readwrite').objectStore('cocktails');
+        favCocktails.add(cocktail);
     }
 
     getCoctailsFromLS(){
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-This project is build on newer java script concepts: classes, async/await functions, fetch api, local storage..
-
-Depending on the hidden input field's value, search-form submits different Rest API queries by name, ingredients, category and so on.
+Main app.js module uses instances of 3 classes: 
+    * cocktailAPI - to send GET HTTP requests by fetch api for cocktails
+    * ui - to create templates for each cocktail with details or without details where bootstrap modal plugin shows details
+    * cocktailDB - to save/remove cocktails from Browser Storage(local storage, session storage, cookies and indexedDB)
